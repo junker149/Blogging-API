66c9419f75ebf05b480a658c39486125b89e48e5
junker149 <rawalaman.0505@gmail.com>
2024-11-28 11:07:15 +0530

    Updated get route to retrieve single post from database as well as all posts

--- src/Routes/posts.ts
+++ src/Routes/posts.ts
@@ -1,30 +1,65 @@
 import express from 'express';
+import { post } from '../validation/postSchema';
 const Router = express.Router();
 import Client from '../client';
 
-Router.post('/post', async (req, res) => {
+Router.post('/posts', async (req, res) => {
     const {title, content, category, tags} = req.body;
-    try{
-        const newPost = await Client.posts.create({
-            data: {title, content, category, tags}
-        });    
-        res.status(200).json({
-            message: newPost
+    const parsedBody = post.safeParse(req.body);
+
+    if (!parsedBody.success) {
+        res.status(400).json({
+            message: 'Bad request'
         })
     }
-    catch(err){
-        res.status(500).json({
-            message: 'Internal Server Error'
-        });
+    else{
+        try{
+            const newPost = await Client.posts.create({
+                data: {title, content, category, tags}
+            });    
+            res.status(201).json({
+                message: 'Created',
+                newPost
+            })
+        }
+        catch(err){
+            res.status(500).json({
+                message: 'Internal Server Error'
+            });
+        }
     }
 })
 
-Router.get('/post', async (req, res) => {
+Router.get('/posts/:id?', async (req, res) => {
+    const id = req.params.id;
     try{
-        const allPosts = await Client.posts.findMany();
-        res.status(200).json({
-            message: allPosts
-        })
+        if (!id) {
+            const allPosts = await Client.posts.findMany();
+            res.status(200).json({
+                message: 'OK',
+                allPosts
+            });
+        }
+        else {
+            const post = await Client.posts.findUnique({
+                where: {
+                    id: parseInt(id)
+                }
+            })
+
+            if (!post){
+                res.status(404).json({
+                    message: 'Not Found'
+                });
+            }
+
+            else {
+                res.status(200).json({
+                    message: 'OK',
+                    post
+                })
+            }
+        }
     }
     catch(err){
         res.status(500).json({
